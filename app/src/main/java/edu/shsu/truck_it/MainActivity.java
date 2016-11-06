095fcb53eba6be9b7548b355041c07a0bc961bc3
Forrest Red <flr005@shsu.edu>
2016-11-05 19:29:20 -0500

    -Added Registration Screen -TextViews below buttons now take user to and from registration activity by clicking on them.

--- app/src/main/java/edu/shsu/truck_it/MainActivity.java
+++ app/src/main/java/edu/shsu/truck_it/MainActivity.java
@@ -1,14 +1,36 @@
 package edu.shsu.truck_it;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private TextView creatAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.button);
+
+        creatAccount = (TextView) findViewById(R.id.textView2);
+
+        creatAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/edu/shsu/truck_it/RegisterActivity.java
@@ -0,0 +1,26 @@
+package edu.shsu.truck_it;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView loginText = (TextView) findViewById(R.id.textView);
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
