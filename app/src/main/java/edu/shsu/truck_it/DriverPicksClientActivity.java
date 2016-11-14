2d87684bb2b558678073a21d61c78d05bff77358
Jude <stovalljr@gmail.com>
2016-11-14 11:03:11 -0600

    Added an activity for drivers selecting jobs.

---
+++ app/src/main/java/edu/shsu/truck_it/DriverPicksClientActivity.java
@@ -0,0 +1,13 @@
+package edu.shsu.truck_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverPicksClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_picks_client);
+    }
+}
