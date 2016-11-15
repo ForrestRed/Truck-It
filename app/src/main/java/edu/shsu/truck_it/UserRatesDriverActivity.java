d48f5e8ccd5cb6fafc050b3f084e50dbf84023d8
Jude <stovalljr@gmail.com>
2016-11-15 13:15:23 -0600

    created user rates driver activity. incomplete

---
+++ app/src/main/java/edu/shsu/truck_it/UserRatesDriverActivity.java
@@ -0,0 +1,13 @@
+package edu.shsu.truck_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRatesDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_rates_driver);
+    }
+}
