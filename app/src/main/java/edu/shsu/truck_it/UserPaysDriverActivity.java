a7abaff6009fc1486061a2368cb526c657c7f077
Jude <stovalljr@gmail.com>
2016-11-29 18:37:41 -0600

    should send a default email to two hardcoded emails onClick of pay driver button

--- app/src/main/java/edu/shsu/truck_it/UserPaysDriverActivity.java
+++ app/src/main/java/edu/shsu/truck_it/UserPaysDriverActivity.java
@@ -2,11 +2,17 @@ package edu.shsu.truck_it;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+import java.util.Arrays;
+import android.util.Log;
+import android.content.Intent;
 
 public class UserPaysDriverActivity extends AppCompatActivity {
-    String passedVar;
+    String passedVar, userEmail, driverEmail;
     DatabaseHelper myDB;
     private int passedTripID;
     private TextView driverName, vehicleType, distanceTraveled, totalCharge;
@@ -36,5 +42,34 @@ public class UserPaysDriverActivity extends AppCompatActivity {
         vehicleType.setText(Integer.toString(finishedJob._vehicleType));
         distanceTraveled.setText(String.valueOf(finishedJob._distance));
         totalCharge.setText(String.valueOf(jobFinalCharge));
+
+        payDriver.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(UserPaysDriverActivity.this, "Payment complete. Thank you for using TruckIt!", Toast.LENGTH_LONG).show();
+                ////////////////////////////////////////////////////////////////////////////////
+                //These lines of code test email function///////////////////////////////////////
+
+
+                Log.i("SendMailActivity", "Send Button Clicked.");
+
+                String fromEmail = "teamharambeshsu@gmail.com";
+                String fromPassword = "Burris4317";
+                String toEmails = "stovalljr@gmail.com , teamharmbeshsu@gmail.com";
+
+                List toEmailList = Arrays.asList(toEmails
+                        .split("\\s*,\\s*"));
+                Log.i("SendMailActivity", "To List: " + toEmailList);
+
+                String emailSubject = "donotreply harambe job confirmation";
+                String emailBody = "Thank you for using TruckIt!";
+                new SendMailTask(UserPaysDriverActivity.this).execute(fromEmail,
+                        fromPassword, toEmailList, emailSubject, emailBody);
+
+                //Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity(i);
+                ////////////////////////////////////////////////////////////////////////////////
+            }
+        });
     }
 }
